fd1c590b0f0c0d3348f35c051fc0dc556d0bd22f
ucwong <ucwong@126.com>
2022-03-23 02:33:18 +0800

    restful

---
+++ cmd/sign/app.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/ucwong/sign/util"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/sign", sign)
+	err := http.ListenAndServe("127.0.0.1:8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func sign(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%v %v %v", r.Method, r.URL, time.Now().Unix())
+	res := "OK"
+
+	q := r.URL.Query()
+	switch r.Method {
+	case "GET":
+		// TODO
+	case "POST":
+		if !util.Verify(q.Get("msg"), q.Get("addr"), q.Get("sig")) {
+			res = "Invalid signature"
+			break
+		}
+		log.Printf("suc\n")
+	default:
+		res = "method not found"
+	}
+	fmt.Fprintf(w, res)
+}
